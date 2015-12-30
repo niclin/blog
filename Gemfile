f06a32b1e9e44210ac979ba504fc7b8af62f9729
niclin <bboyceo@hotmail.com>
2015-12-30 20:38:35 +0800

    Fix user_login

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'devise'
 gem 'i18n'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @posts = Post.all.order('created_at DESC')
@@ -43,6 +44,7 @@ class PostsController < ApplicationController
 
 
   private
+
   def post_params
     params.require(:post).permit(:title, :body)
   end
